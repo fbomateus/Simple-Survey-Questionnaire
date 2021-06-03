81d30794d90b21e8697f414e96b87c3ade76ecc3
Fábio Mateus <fabio@avanz.com.br>
2021-06-03 04:38:51 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/simple_survey_questionnaire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_survey_questionnaire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(SurveyQuetionnaireApp());
+
+class SurveyQuetionnaireApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello Flutter!!!'),
+    );
+  }
+}
