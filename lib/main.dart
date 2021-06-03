9226a830f0fb5b44223338b7a1784f8409e3ec49
Fábio Mateus <fabiomateus.jobs@gmail.com>
2021-06-03 05:49:46 -0300

    changing stateless class to stateful and adding response options

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,43 @@ import 'package:flutter/material.dart';
 
 main() => runApp(SurveyQuetionnaireApp());
 
-class SurveyQuetionnaireApp extends StatelessWidget {
+class SurveyQuetionnaireAppState extends State<SurveyQuetionnaireApp> {
+  var selectedQuestion = 0;
+
+  void answer() {
+    setState(() {
+      selectedQuestion++;
+    });
+    print(selectedQuestion);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final questions = [
+      "What's your favourite colour?",
+      "What's your favourite animal?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Questions'),
         ),
-        body: Text('Hello Flutter!!!'),
+        body: Column(
+          children: [
+            Text(questions[selectedQuestion]),
+            ElevatedButton(onPressed: answer, child: Text('Answer 01')),
+            ElevatedButton(onPressed: answer, child: Text('Answer 02')),
+            ElevatedButton(onPressed: answer, child: Text('Answer 03')),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class SurveyQuetionnaireApp extends StatefulWidget {
+  SurveyQuetionnaireAppState createState() {
+    return SurveyQuetionnaireAppState();
   }
 }
